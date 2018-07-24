9240c8d02b143fc912d55062a00b5c8cec9a1a47
Hackbright Student <no-reply@hackbrightacademy.com>
2018-07-23 20:45:21 -0700

    further study

---
+++ test.py
@@ -0,0 +1,8 @@
+import server
+
+print (__name__)
+
+if __name__ == '__main__':
+    # debug=True gives us error messages in the browser and also "reloads"
+    # our web app if we change the code.
+    app.run(debug=True)
